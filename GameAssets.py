75442962ab5a14e9d5852529ea8393d665c67c41
ClaasLeuphana <166377197+ClaasLeuphana@users.noreply.github.com>
2024-07-08 22:36:30 +0200

    Grafiken Part 1

--- GameAssets.py
+++ GameAssets.py
@@ -1,5 +1,4 @@
 import pygame
-import Grafiken
 import os
 
 
@@ -10,22 +9,22 @@ def load_image(filename):
 class GameAssets:
     # Laden der Bilder
     Board = load_image("Board.png")
-    CardBack = load_image("CardBack.png")
+    CardBack = load_image("Back of Card.png")
     Card12 = load_image("Card12.png")
     Card11 = load_image("Card11.png")
     Card10 = load_image("Card10.png")
-    Card9 = load_image("Card9.png")
+    Card9 = load_image("Playincard 9.png")
     Card8 = load_image("Card8.png")
     Card7 = load_image("Card7.png")
     Card6 = load_image("Card6.png")
-    Card5 = load_image("Card5.png")
+    Card5 = load_image("Playingcard 5.png")
     Card4 = load_image("Card4.png")
     Card3 = load_image("Card3.png")
-    Card2 = load_image("Card2.png")
-    Card1 = load_image("Card1.png")
-    Card0 = load_image("Card0.png")
-    CardN1 = load_image("CardN1.png")
-    CardN2 = load_image("CardN2.png")
+    Card2 = load_image("Playingcard 2.png")
+    Card1 = load_image("Playingcard 1.png")
+    Card0 = load_image("Playingcard 0.png")
+    CardN1 = load_image("Playingcard -1.png")
+    CardN2 = load_image("Playingcard -2.png")
         # draw animation
         # flip animation
         # deal animation
